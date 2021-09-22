b90753287b43abe0557c0af9e24cee410a1b3dc1
Adam Nitti <adamnitti@gmail.com>
2021-09-22 12:46:39 -0500

    Refined JS code

--- js-interview-copy.js
+++ js-interview-copy.js
@@ -17,33 +17,76 @@ const drawRequests = [
     { drawId: 6, itemId: 3, amount: 50000, effectiveDate: '10/7/2015' },
     { drawId: 7, itemId: 2, amount: 1000, effectiveDate: '11/16/2015' },
     { drawId: 8, itemId: 16, amount: 750, effectiveDate: '11/15/2015' },
-    { drawId: 10, itemId: 3, amount: -1, effectiveDate: '10/5/2015' },
     { drawId: 9, itemId: 1, amount: 2000, effectiveDate: 123345 },
+    { drawId: 10, itemId: 3, amount: -1, effectiveDate: '10/5/2015' },
 ];
 
 function processDraws(drawRequests, budget) {
     //@todo implement solution here
 
     const numBudgetItems = budget.budgetItems.length;
-    const resultsArray = { successfulDraws: [], unsuccessfulDraws: [] };
+    let budgetItemRemainder = 0;
+    const resultsArray = {
+        sucessfulDraws: [],
+        unsuccessfulDraws: [],
+        successfulSorts: [],
+        unsuccessfulSorts: [],
+    };
 
+    // Call checkErrors function and sort draw requests
+    console.log('checking errors on draw requests:', drawRequests);
     checkErrors(drawRequests);
+    console.log('successful sorts:', resultsArray.successfulSorts);
+    console.log('unsuccessful sorts:', resultsArray.unsuccessfulSorts);
 
-    attemptDraw(resultsArray.successfulDraws);
+    // Call attemptDraw function to begin transaction attempts
+    attemptDraw(resultsArray.successfulSorts);
 
+    // Attempt transactions/draws and process or refuse accordingly
     function attemptDraw(item) {
-        console.log(resultsArray.successfulDraws);
-        item.forEach((draw) => {
-            for (i = 0; i < numBudgetItems; i++) {
-                const budgetItem = budget.budgetItems;
-                let itemRemainder =
-                    budgetItem[i].originalAmount - budgetItem[i].fundedToDate;
-                console.log('in for loop');
-            }
-            //console.log('attempting draw', draw);
+        console.log('num budget items=', numBudgetItems);
+
+        // Iterate through total number of budget items
+        budget.budgetItems.forEach((budgetItem) => {
+            budgetItemRemainder =
+                budgetItem.originalAmount - budgetItem.fundedToDate;
+            console.log(
+                '\n',
+                'initial budgetItemRemainder=',
+                budgetItemRemainder
+            );
+            const budgetItemFiltered = resultsArray.successfulSorts
+                .filter((item) => item.itemId == budgetItem.itemId)
+                .sort(
+                    (a, b) =>
+                        new Date(a.effectiveDate) - new Date(b.effectiveDate)
+                );
+            console.log('budgetItemFiltered = ', budgetItemFiltered);
+
+            // Iterate through transaction attempts that match id number for each budget
+            budgetItemFiltered.forEach((item) => {
+                budgetItemRemainder = budgetItemRemainder - item.amount;
+                console.log('attempting draw of', item.amount);
+                if (budgetItemRemainder < 0) {
+                    console.log(
+                        'insufficient funds. draw rejected. remainder =',
+                        budgetItemRemainder + item.amount
+                    );
+                    budgetItemRemainder = budgetItemRemainder + item.amount;
+                    resultsArray.unsuccessfulDraws.push(item);
+                } else {
+                    console.log(
+                        'successful draw. remainder:',
+                        budgetItemRemainder
+                    );
+                    resultsArray.sucessfulDraws.push(item);
+                }
+            });
+            //console.log('final remainder =', budgetItemRemainder);
         });
     }
 
+    // Check for errors in draw requests
     function checkErrors(request) {
         request.forEach((item) => {
             let itemIndex = request.indexOf(item);
@@ -58,33 +101,26 @@ function processDraws(drawRequests, budget) {
                     'Draw not processed due to invalid itemId',
                     item.itemId
                 );
-
-                console.log('itemIndex = ', itemIndex);
-                resultsArray.unsuccessfulDraws.push(item);
+                resultsArray.unsuccessfulSorts.push(item);
             } else if (item.amount < 0 || !(typeof item.amount == 'number')) {
                 console.log(
                     'Draw not processed due to invalid draw amount',
                     item.amount
                 );
-
-                console.log('itemIndex = ', itemIndex);
-                resultsArray.unsuccessfulDraws.push(item);
+                resultsArray.unsuccessfulSorts.push(item);
             } else if (!validateDate(item.effectiveDate)) {
                 console.log(
                     'Draw not processed due to invalid date format',
                     item.effectiveDate
                 );
-
-                console.log('itemIndex = ', itemIndex);
-                resultsArray.unsuccessfulDraws.push(item);
+                resultsArray.unsuccessfulSorts.push(item);
             } else {
-                resultsArray.successfulDraws.push(item);
-                resultsArray.successfulDraws.sort(
+                resultsArray.successfulSorts.push(item);
+                resultsArray.successfulSorts.sort(
                     (a, b) =>
                         new Date(a.effectiveDate) - new Date(b.effectiveDate)
                 );
             }
-            //console.log(resultsArray);
         });
     }
 
@@ -95,8 +131,8 @@ function processDraws(drawRequests, budget) {
         return date_regex.test(testdate);
     }
 
-    return drawRequests;
+    return resultsArray;
 }
 
 const results = processDraws(drawRequests, budget);
-//console.log(JSON.stringify(results, null, 2));
+console.log(JSON.stringify(results, null, 2));
